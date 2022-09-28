583e4a3156cdbfb8aa4ea7326d038b93415fc752
Karlov  Mark <karlovma@ug215.eecg.toronto.edu>
2022-09-28 11:18:04 -0400

    refactoring of section 2

---
+++ TWO_AGAIN/deliver.c
@@ -0,0 +1,171 @@
+/*
+** client.c -- a stream socket client demo
+** Taken from Beej's Guide to Network Programming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAX_PACKET_BUFFER_SIZE 1500
+void ftp(int sockfd,struct addrinfo *p,char* filename);
+
+struct packet{
+	unsigned int total_frag;
+
+	unsigned int frag_no;
+	unsigned int size;
+	char* filename;
+	char filedata[1000];
+};
+
+struct frame{
+	int frame_kind;
+	int sq_no;
+	int ack;
+};
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    if (argc != 3) {
+        fprintf(stderr,"usage: client hostname message\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to create socket\n");
+        return 2;
+    }
+
+    char cmd[128];
+    char filename[128];
+    scanf("%s %s",cmd,filename);
+
+    if(!strcmp("ftp",cmd)){
+	if(access(filename, F_OK) == 0){
+		//file does exist
+	    if ((numbytes = sendto(sockfd, "ftp", 3, 0, p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("client: sendto");
+		exit(1);
+	    }
+
+	    char response[4];
+	    socklen_t addr_len = sizeof p->ai_addr;
+	    if ((numbytes = recvfrom(sockfd, response, 3 , 0,
+		p->ai_addr, &addr_len)) == -1){
+		perror("recvfrom");
+		exit(1);
+	    }
+	    response[numbytes] = '\0';
+	    if (!strcmp(response, "yes")){
+		printf("A file transfer can start\n");
+		ftp(sockfd,p,filename);
+	    }
+	    else{ // no
+		exit(1); 
+	    }
+	}
+	else{
+		//file doesnt exist
+		printf("File doesnt exist\n");
+		exit(1);
+	}
+    }
+    else{
+	    perror("command: invalid");
+	    exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+    close(sockfd);
+    return 0;
+}
+
+void ftp(int sockfd,struct addrinfo *p,char* filename){
+	struct packet* Packet = (struct packet*) malloc(sizeof(struct packet));
+	struct frame* Frame = (struct frame*) malloc(sizeof(struct frame));
+	char* packetBuf = (char*) malloc(MAX_PACKET_BUFFER_SIZE);
+	memset(Packet, 0, sizeof(struct packet));
+	memset(Frame, 0, sizeof(struct frame));
+
+	FILE* f = fopen(filename,"rb");
+	fseek(f, 0L, SEEK_END);
+	int sz = ftell(f);
+	rewind(f);
+	Packet->total_frag = ((int) (sz - 1)/1000) + 1;
+	Packet->filename,filename);
+	Packet->frag_no = 1;
+
+	int numbytes = 0,ack_recv = 1;
+
+	while(1){
+		if(ack_recv == 1){
+			if(!feof(f)){
+				fread(Packet->filedata,1,1000,f);
+				Packet->size = ((sz - 1000*(Packet->frag_no-1)) >= 1000) ? 1000 : (sz - 1000*(Packet->frag_no-1));
+				sprintf(packetBuf,"%d:%d:%d:%s:", Packet->total_frag, Packet->frag_no, Packet->size, Packet->filename);
+				int headerSize = strlen(packetBuf);
+				memcpy(packetBuf+strlen(packetBuf),Packet->filedata,1000);
+
+				if ((numbytes = sendto(sockfd, packetBuf, Packet->size + headerSize, 0, p->ai_addr, p->ai_addrlen)) == -1) {
+					perror("client: sendto");
+					exit(1);
+				}
+				printf("Sent Packet #%d\n",Packet->frag_no);
+
+				memset(Packet->filedata,0,1000);
+			}
+			else break;
+		}
+
+		socklen_t addr_len = sizeof p->ai_addr;
+		if((numbytes = recvfrom(sockfd, Frame, sizeof(struct frame), 0, p->ai_addr, &addr_len)) == -1){
+			perror("recvfrom");
+			exit(1);
+		}
+
+		if(numbytes > 0 && Frame->sq_no == 0 && Frame->recv.ack == Packet->frag_no){
+			printf("ACK Received for Packet #%d\n",Packet->frag_no);
+			ack_recv = 1;
+		}
+		else{
+			printf("ACK NOT Received for Packet #%d\n",Packet->frag_no);
+			ack_recv = 0;
+		}
+
+		Packet->frag_no++;
+	}
+}
+
---
+++ TWO_AGAIN/server.c
@@ -0,0 +1,144 @@
+/*
+** server.c -- a stream socket server demo
+** take from Beej's Guide to Network Programming
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+//#define MYPORT "4950"    // the port users will be connecting to
+
+#define MAX_PACKET_BUFFER_SIZE 1500
+
+// get sockaddr, IPv4 or IPv6:
+struct packet{
+	unsigned int total_frag;
+
+	unsigned int frag_no;
+	unsigned int size;
+	char* filename;
+	char filedata[1000];
+};
+
+struct frame{
+	int frame_kind;
+	int sq_no;
+	int ack;
+};
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc,char* argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("server: waiting to recvfrom...\n");
+
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+    printf("server: packet contains \"%s\"\n", buf);
+    
+    char ftp[] = "ftp";
+    if (!strcmp(buf, ftp)){
+        printf("server: return message \"yes\"\n");
+        if ((numbytes = sendto(sockfd, "yes", 3, 0,
+            (struct sockaddr *)&their_addr, addr_len)) == -1){
+            perror("server: sendto");
+            exit(1);
+        }
+
+	void ftp(sockfd,&their_addr);
+    }
+    else{
+        printf("server: return message \"no\"\n");
+        if ((numbytes = sendto(sockfd, "no", 3, 0,
+            (struct sockaddr *)&their_addr, addr_len)) == -1){
+            perror("server: sendto");
+            exit(1);
+        }
+    }
+
+
+    close(sockfd);
+
+    return 0;
+}
+
+void(int sockfd,struct sockaddr_storage* their_addr){
+	struct packet* Packet = (struct packet*) malloc(sizeof(struct packet));
+	struct frame* Frame = (struct frame*) malloc(sizeof(struct frame));
+	char* packetBuf = (char*) malloc(MAX_PACKET_BUFFER_SIZE);
+	memset(Packet, 0, sizeof(struct packet));
+	memset(Frame, 0, sizeof(struct frame));
+
+	while(1){
+		socklen_t addr_len = sizeof(*their_addr);
+		if((numbytes = recvfrom(sockfd, packetBuf, MAX_PACKET_BUFFER_SIZE, 0, (struct sockaddr *) &(*their_addr), &addr_len)) == -1){
+			perror("recvfrom");
+			exit(1);
+		}
+		
+	}
+
